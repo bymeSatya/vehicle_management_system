ce52917df9765d946101dc6e09f02b621596ec71
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2023-10-09 19:46:41 +0530

    Update login.py

--- login.py
+++ login.py
@@ -11,8 +11,9 @@ import time
 st.set_page_config()
 count_down = st.number_input("Enter")
 ph = st.empty()
-N = count_down*60
-for secs in range(N,0,-1):
-    mm, ss = secs//60, secs%60
-    ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
-    time.sleep(1)
+def cnt_down(count_down):
+    N = count_down*60
+    for secs in range(N,0,-1):
+        mm, ss = secs//60, secs%60
+        ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
+        time.sleep(1)
