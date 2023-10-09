da5de60ab1a66bd46b7271d61b2ce0c5426737d1
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2023-10-09 19:24:53 +0530

    Update login.py

--- login.py
+++ login.py
@@ -6,3 +6,19 @@ import requests
 
 streamlit.title('User Login')
 
+timer = 10
+
+# Create a function that will be called every second
+def count_down():
+  # Decrement the timer by 1
+  timer -= 1
+
+  # Update the timer text
+  st.write(f"Time remaining: {timer} seconds")
+
+  # If the timer has reached zero, stop counting
+  if timer == 0:
+    return
+
+# Start the timer
+asyncio.run(periodic(count_down, 1))
