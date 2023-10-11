5c21b6a0bc4532dfa14b627d02b948daa27bc37f
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2023-10-11 17:24:24 +0530

    Update login.py

--- login.py
+++ login.py
@@ -6,16 +6,15 @@ import requests
 
 #streamlit.title('User Login')
 
-import time
+import datetime,pytz
 
-st.set_page_config()
-count_down = st.number_input("Enter")
-ph = st.empty()
-cnt_down(count_down)
-def cnt_down(count_down):
-    N = count_down*60
-    for secs in range(N,0,-1):
-        mm, ss = secs//60, secs%60
-        ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
-        time.sleep(1)
+dtobj1=datetime.datetime.utcnow()   #utcnow class method
+print(dtobj1)
+
+dtobj3=dtobj1.replace(tzinfo=pytz.UTC) #replace method
+
+
+#print(pytz.all_timezones) => To see all timezones
+dtobj_india=dtobj3.astimezone(pytz.timezone("Asia/Calcutta")) #astimezone method
+print(dtobj_india)
 
