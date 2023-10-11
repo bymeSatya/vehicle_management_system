d6b8dc55ffc3265e93292eced79c72c5b70b5d60
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2023-10-11 17:30:52 +0530

    Update login.py

--- login.py
+++ login.py
@@ -6,7 +6,7 @@ import requests
 
 #streamlit.title('User Login')
 
-import datetime,pytz
+import datetime,pytz,time
 
 dtobj1=datetime.datetime.utcnow()   #utcnow class method
 print(dtobj1)
@@ -16,5 +16,7 @@ dtobj3=dtobj1.replace(tzinfo=pytz.UTC) #replace method
 
 #print(pytz.all_timezones) => To see all timezones
 dtobj_india=dtobj3.astimezone(pytz.timezone("Asia/Calcutta")) #astimezone method
-st.write(dtobj_india)
+for i in range(1,100):
+  time.sleep(i)
+  st.write(dtobj_india)
 
